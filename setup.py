1f260478ab5aab8dd0b5229f726c7e32c9081e66
Matthew Altberg <matthew.altberg@strong.io>
2022-12-06 12:19:16 +1300

    Switch to using pyproject.toml to build wheels

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
+import itertools
+import os
 import setuptools
-from bootcamp import __version__
+import sys
+
+setup_kwargs = {}
+
+if 'develop' in sys.argv:
+    setup_kwargs['packages'] = [p for p in setuptools.find_packages() if 'bootcamp' in p]
+
+else:
+    from Cython.Build import cythonize
+    from Cython.Distutils import build_ext
+
+    # declare Cythonized extensions
+    extensions = list(itertools.chain.from_iterable(
+        [
+            setuptools.Extension('{}.{}'.format(p, f.replace('.py', '')), [os.path.join(p.replace('.', '/'), f)])
+            for f in os.listdir(p.replace('.', '/')) if f[-3:] == '.py'
+        ]
+        for p in setuptools.find_packages() if 'bootcamp' in p
+    ))
+
+    setup_kwargs['ext_modules'] = cythonize(
+        extensions, language_level="3", build_dir="build", compiler_directives=dict(always_allow_keywords=True)
+    )
+    setup_kwargs['cmdclass'] = {'build_ext': build_ext}
+    setup_kwargs['packages'] = []
 
 setuptools.setup(
-    name='strong-bootcamp',
-    version=__version__,
-    packages=[p for p in setuptools.find_packages() if 'bootcamp' in p],
-    url='https://www.strong.io',
-    license='',
-    author='Strong Analytics',
-    author_email='contact@strong.io',
-    description='Framework for experimenting with and validating models pre-deployment.',
-    install_requires=['pyyaml'],
-    entry_points={
-        'console_scripts': [
-            'bootcamp=bootcamp.cli:main'
-        ]
-    }
+    **setup_kwargs
 )
